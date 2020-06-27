c6b83f3843966437d16d8bde7f3a5c7ad85165bd
Darek <darsel@wp.pl>
2020-06-27 15:39:13 +0200

    dodane otwieranie zdjęć

---
+++ src/actions.js
@@ -0,0 +1,8 @@
+export const shouldShowLoaderAction = {
+  type: 'SHOULD_SHOW_LOADER'
+}
+
+export const setList = (list) => ({
+  type: 'SET_LIST',
+  payload: list
+});
---
+++ src/components/Sections/index.js
@@ -0,0 +1,5 @@
+/* import Sections from './Sections';
+
+export default Sections; */
+
+export { default } from './SectionsContainer'
